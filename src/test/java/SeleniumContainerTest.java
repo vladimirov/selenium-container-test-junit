cbcaa4f92d72d7e7d71809990a04b1d07fd1c81d
vladimirov <denis.vladimiroff@gmail.com>
2018-10-24 18:06:09 +0300

    project name changed

--- src/test/java/SeleniumContainerTest.java
+++ src/test/java/SeleniumContainerTest.java
@@ -22,10 +22,18 @@ public class SeleniumContainerTest {
 
     @Test
     public void simplePlainSeleniumTest() throws InterruptedException {
-        RemoteWebDriver driver = chrome.getWebDriver();
 
-        driver.get("http://localhost/wordpress-test/");
+        RemoteWebDriver driver = chrome.getWebDriver();
+//        driver.get("http://localhost/wordpress-test/");
+//        driver.get("http://10.11.1.188");
+        driver.get("http://wordpress-test/");
         Thread.sleep(3000);
+
+        System.out.println(chrome.getPassword());
+        System.out.println(chrome.getPort());
+        System.out.println(chrome.getSeleniumAddress());
+        System.out.println(chrome.getVncAddress());
+
 //        WebElement scrollButton = driver.findElementByClassName("menu-scroll-down");
 //        scrollButton.click();
 //        WebElement searchInput = driver.findElementByClassName("search-form");
