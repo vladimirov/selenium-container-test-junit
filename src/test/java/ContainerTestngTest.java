88bdc769e79029d374c0002b718e08b0af257f01
vladimirov <denis.vladimiroff@gmail.com>
2018-10-29 17:35:54 +0200

    added two types of tests

--- src/test/java/ContainerTestngTest.java
+++ src/test/java/ContainerTestngTest.java
@@ -1,26 +1,32 @@
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
+
 
 import java.io.File;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
-/**
- * Simple example of plain Selenium usage.
- */
-public class SeleniumContainerTest {
+public class ContainerTestngTest {
 
-    @Rule
-    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+    public static BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
             .withDesiredCapabilities(DesiredCapabilities.chrome())
             .withRecordingMode(RECORD_ALL, new File("target"));
 
+    @BeforeClass
+    public static void setUp(){
+        chrome.start();
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        chrome.stop();
+    }
+
     @Test
-    public void simplePlainSeleniumTest() throws InterruptedException {
+    public void containerTest() throws InterruptedException {
         RemoteWebDriver driver = chrome.getWebDriver();
         driver.get("http://wordpress.local/");
         driver.findElementById("menu-item-388").click();
@@ -30,4 +36,5 @@ public class SeleniumContainerTest {
                 .anyMatch(element -> element.getText().contains("this"));
         assertTrue("The word 'this' is found on a page", expectedTextFound);
     }
+
 }
