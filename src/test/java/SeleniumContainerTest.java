6fb12c0a705c15618db0d403befd167e3c4fac72
vladimirov <denis.vladimiroff@gmail.com>
2018-11-01 11:01:06 +0200

    updated working example

--- src/test/java/SeleniumContainerTest.java
+++ src/test/java/SeleniumContainerTest.java
@@ -1,29 +1,20 @@
-import org.junit.*;
+import org.junit.Rule;
+import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-
 
 import java.io.File;
 
 import static org.rnorth.visibleassertions.VisibleAssertions.assertTrue;
 import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
-public class ContainerTestngTest {
+public class SeleniumContainerTest {
 
-    public static BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+    @Rule
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
             .withDesiredCapabilities(DesiredCapabilities.chrome())
             .withRecordingMode(RECORD_ALL, new File("target"));
-
-    @BeforeClass
-    public static void setUp(){
-        chrome.start();
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        chrome.stop();
-    }
 
     @Test
     public void containerTest() throws InterruptedException {
